699c86cabacfcd67ecc6f856d445ddffc8c2dce8
Eloy Romero <eromero@jlab.org>
2020-12-03 15:23:58 -0500

    Add serius testing in the local test.

--- tests/local.cpp
+++ tests/local.cpp
@@ -10,72 +10,157 @@
 using namespace superbblas;
 
 int main(void) {
+    constexpr unsigned int Nd = 6; // xyztsc
+    //const Coor<Nd> dim = {2, 2, 2, 2, 2, 2};
+    const Coor<Nd> dim = {16, 16, 16, 16, 16, 16};
+    //using T = double;
+    using T = std::complex<double>;
     {
-        constexpr unsigned int Nd = 1;
-        using LatticeCoor = Coor<Nd>;
-        //using Tensor = std::vector<std::complex<double>>;
-        using Tensor = std::vector<double>;
-        const LatticeCoor dim = {5};
-        unsigned int vol0 = detail::volume<Nd>(dim), vol1 = vol0;
+        using Tensor = std::vector<T>;
+        const Coor<Nd - 1> dim0 = {dim[0], dim[1], dim[2], dim[3], dim[4]}; // xyzts
+        const Coor<Nd> dim1 = {dim[3], dim[5], dim[0], dim[1], dim[2], dim[4]}; // tcxyzs
+        unsigned int vol0 = detail::volume<Nd-1>(dim0);
+        unsigned int vol1 = detail::volume<Nd>(dim1);
         Tensor t0(vol0), t1(vol1);
         for (unsigned int i = 0; i < vol0; i++) t0[i] = i;
-        const LatticeCoor zero_coor = {0};
-        const LatticeCoor dim1 = {5};
         Context ctx = createCpuContext();
+
+        std::cout << ">>> CPU tests with " << omp_get_max_threads() << " threads" << std::endl;
+
+        std::cout << "Maximum number of elements in a tested tensor: " << vol1 << " ( "
+                  << vol1 * 1.0 * sizeof(T) / 1024 / 1024 << " MiB)" << std::endl;
+
+        // Copy tensor t0 into tensor 1 (for reference)
         {
             double t = omp_get_wtime();
             for (unsigned int rep = 0; rep < 10; ++rep) {
-                local_copy<Nd, Nd>("x", zero_coor, dim, dim, t0.data(), ctx, "x", zero_coor, dim1,
-                                   t1.data(), ctx);
+                for (int c = 0; c < dim[5]; ++c) {
+#ifdef _OPENMP
+#    pragma omp parallel for
+#endif
+                    for (unsigned int i = 0; i < (unsigned int)vol0; ++i)
+                        t1[i + c * (unsigned int)vol0] = t0[i];
+                }
             }
             t = omp_get_wtime() - t;
-            std::cout << "Time in permuting " << t / 10 << std::endl;
+            std::cout << "Time in dummy copying from xyzts to tcxyzs " << t / 10 << std::endl;
         }
 
-        Tensor tc(1);
+
+        // Copy tensor t0 into each of the c components of tensor 1
         {
             double t = omp_get_wtime();
             for (unsigned int rep = 0; rep < 10; ++rep) {
-                local_contraction<Nd, Nd, 0>("x", dim, false, t0.data(), "x", dim1, false,
-                                             t1.data(), "", {}, tc.data(), ctx);
+                for (int c = 0; c < dim[5]; ++c) {
+                    const Coor<Nd - 1> from0 = {0};
+                    const Coor<Nd> from1 = {0, c, 0};
+                    local_copy<Nd - 1, Nd>("xyzts", from0, dim0, dim0, t0.data(), ctx, "tcxyzs",
+                                           from1, dim1, t1.data(), ctx);
+                }
             }
             t = omp_get_wtime() - t;
-            std::cout << "Time in permuting " << t / 10 << std::endl;
+            std::cout << "Time in copying/permuting from xyzts to tcxyzs " << t / 10 << std::endl;
+        }
+
+        // Shift tensor 1 on the z-direction and store it on tensor 2
+        Tensor t2(vol1);
+         {
+            double t = omp_get_wtime();
+            for (unsigned int rep = 0; rep < 10; ++rep) {
+                const Coor<Nd> from0 = {0};
+                Coor<Nd> from1 = {0};
+                from1[4] = 1; // Displace one on the z-direction
+                local_copy<Nd, Nd>("tcxyzs", from0, dim1, dim1, t1.data(), ctx, "tcxyzs", from1,
+                                       dim1, t1.data(), ctx);
+            }
+            t = omp_get_wtime() - t;
+            std::cout << "Time in shifting " << t / 10 << std::endl;
+        }
+
+        const Coor<3> dimc = {dim[3], dim[5], dim[5]}; // tcc
+        unsigned int volc = detail::volume<3>(dimc); 
+        Tensor tc(volc);
+        {
+            double t = omp_get_wtime();
+            for (unsigned int rep = 0; rep < 10; ++rep) {
+                local_contraction<Nd, Nd, 3>("tcxyzs", dim, false, t1.data(), "tCxyzs", dim1, false,
+                                             t2.data(), "tCc", dimc, tc.data(), ctx);
+            }
+            t = omp_get_wtime() - t;
+            std::cout << "Time in contracting " << t / 10 << std::endl;
         }
     }
 #ifdef SUPERBBLAS_USE_CUDA
     {
-        constexpr unsigned int Nd = 1;
-        using LatticeCoor = Coor<Nd>;
-        //using Tensor = thrust::device_vector<std::complex<double>>;
-        using Tensor = thrust::device_vector<double>;
-        const LatticeCoor dim = {5};
-        unsigned int vol0 = detail::volume<Nd>(dim), vol1 = vol0;
+        using Tensor = thrust::device_vector<T>;
+        const Coor<Nd - 1> dim0 = {dim[0], dim[1], dim[2], dim[3], dim[4]}; // xyzts
+        const Coor<Nd> dim1 = {dim[3], dim[5], dim[0], dim[1], dim[2], dim[4]}; // tcxyzs
+        unsigned int vol0 = detail::volume<Nd-1>(dim0);
+        unsigned int vol1 = detail::volume<Nd>(dim1);
         Tensor t0(vol0), t1(vol1);
-        thrust::counting_iterator<unsigned int> it(0);
-        thrust::copy(it, it + vol0, t0.begin());
-        const LatticeCoor zero_coor = {0};
-        const LatticeCoor dim1 = {5};
+        for (unsigned int i = 0; i < vol0; i++) t0[i] = i;
         Context ctx = createCudaContext();
+
+        std::cout << ">>> GPU tests" << std::endl;
+
+        std::cout << "Maximum number of elements in a tested tensor: " << vol1 << " ( "
+                  << vol1 * 1.0 * sizeof(T) / 1024 / 1024 << " MiB)" << std::endl;
+
+        // Copy tensor t0 into tensor 1 (for reference)
         {
             double t = omp_get_wtime();
             for (unsigned int rep = 0; rep < 10; ++rep) {
-                local_copy<Nd, Nd>("x", zero_coor, dim, dim, t0.data().get(), ctx, "x", zero_coor,
-                                   dim1, t1.data().get(), ctx);
+                for (int c = 0; c < dim[5]; ++c) {
+                    thrust::copy_n(t0.begin(), vol0, t1.begin() + c * vol0);
+                }
             }
             t = omp_get_wtime() - t;
-            std::cout << "Time in permuting " << t / 10 << std::endl;
+            std::cout << "Time in dummy copying from xyzts to tcxyzs " << t / 10 << std::endl;
         }
 
-        Tensor tc(1);
+
+        // Copy tensor t0 into each of the c components of tensor 1
         {
             double t = omp_get_wtime();
             for (unsigned int rep = 0; rep < 10; ++rep) {
-                local_contraction<Nd, Nd, 0>("x", dim, false, t0.data().get(), "x", dim1, false,
-                                             t1.data().get(), "", {}, tc.data().get(), ctx);
+                for (int c = 0; c < dim[5]; ++c) {
+                    const Coor<Nd - 1> from0 = {0};
+                    const Coor<Nd> from1 = {0, c, 0};
+                    local_copy<Nd - 1, Nd>("xyzts", from0, dim0, dim0, t0.data().get(), ctx,
+                                           "tcxyzs", from1, dim1, t1.data().get(), ctx);
+                }
             }
             t = omp_get_wtime() - t;
-            std::cout << "Time in permuting " << t / 10 << std::endl;
+            std::cout << "Time in copying/permuting from xyzts to tcxyzs " << t / 10 << std::endl;
+        }
+
+        // Shift tensor 1 on the z-direction and store it on tensor 2
+        Tensor t2(vol1);
+         {
+            double t = omp_get_wtime();
+            for (unsigned int rep = 0; rep < 10; ++rep) {
+                const Coor<Nd> from0 = {0};
+                Coor<Nd> from1 = {0};
+                from1[4] = 1; // Displace one on the z-direction
+                local_copy<Nd, Nd>("tcxyzs", from0, dim1, dim1, t1.data().get(), ctx, "tcxyzs",
+                                   from1, dim1, t1.data().get(), ctx);
+            }
+            t = omp_get_wtime() - t;
+            std::cout << "Time in shifting " << t / 10 << std::endl;
+        }
+
+        const Coor<3> dimc = {dim[3], dim[5], dim[5]}; // tcc
+        unsigned int volc = detail::volume<3>(dimc); 
+        Tensor tc(volc);
+        {
+            double t = omp_get_wtime();
+            for (unsigned int rep = 0; rep < 10; ++rep) {
+                local_contraction<Nd, Nd, 3>("tcxyzs", dim, false, t1.data().get(), "tCxyzs", dim1,
+                                             false, t2.data().get(), "tCc", dimc, tc.data().get(),
+                                             ctx);
+            }
+            t = omp_get_wtime() - t;
+            std::cout << "Time in contracting " << t / 10 << std::endl;
         }
     }
 #endif
