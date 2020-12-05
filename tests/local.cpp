90343bd68808aa733574b8ac1778cf310fed7705
Eloy Romero <eromero@jlab.org>
2020-12-05 16:46:39 -0500

    Fix bugs in calling local_contraction and add copy with array types

--- tests/local.cpp
+++ tests/local.cpp
@@ -12,9 +12,10 @@ using namespace superbblas;
 int main(void) {
     constexpr unsigned int Nd = 6; // xyztsc
     //const Coor<Nd> dim = {2, 2, 2, 2, 2, 2};
-    const Coor<Nd> dim = {16, 16, 16, 16, 16, 16};
+    constexpr unsigned int SC = 12; // length of dimension SC
+    const Coor<Nd> dim = {16, 16, 16, 32, SC, 64};
     //using T = double;
-    using T = std::complex<double>;
+    using T = std::complex<float>;
     {
         using Tensor = std::vector<T>;
         const Coor<Nd - 1> dim0 = {dim[0], dim[1], dim[2], dim[3], dim[4]}; // xyzts
@@ -31,6 +32,7 @@ int main(void) {
                   << vol1 * 1.0 * sizeof(T) / 1024 / 1024 << " MiB)" << std::endl;
 
         // Copy tensor t0 into tensor 1 (for reference)
+        double tref = 0.0;
         {
             double t = omp_get_wtime();
             for (unsigned int rep = 0; rep < 10; ++rep) {
@@ -44,6 +46,7 @@ int main(void) {
             }
             t = omp_get_wtime() - t;
             std::cout << "Time in dummy copying from xyzts to tcxyzs " << t / 10 << std::endl;
+            tref = t / 10; // time in copying a whole tensor with size dim1
         }
 
 
@@ -59,7 +62,29 @@ int main(void) {
                 }
             }
             t = omp_get_wtime() - t;
-            std::cout << "Time in copying/permuting from xyzts to tcxyzs " << t / 10 << std::endl;
+            std::cout << "Time in copying/permuting from xyzts to tcxyzs " << t / 10
+                      << " (overhead " << t / 10 / tref << " )" << std::endl;
+        }
+
+        // Copy tensor t0 into each of the c components of tensor 1 (fast)
+        {
+            double t = omp_get_wtime();
+            for (unsigned int rep = 0; rep < 10; ++rep) {
+                for (int c = 0; c < dim[5]; ++c) {
+                    const Coor<Nd - 2> from0 = {0};
+                    const Coor<Nd - 1> from1 = {0, c, 0};
+                    Coor<Nd - 2> dim0a;
+                    std::copy_n(dim0.begin(), Nd - 2, dim0a.begin());
+                    Coor<Nd - 1> dim1a;
+                    std::copy_n(dim1.begin(), Nd - 1, dim1a.begin());
+                    local_copy<Nd - 2, Nd - 1>("xyzt", from0, dim0a, dim0a,
+                                               (const std::array<T, SC> *)t0.data(), ctx, "tcxyz",
+                                               from1, dim1a, (std::array<T, SC> *)t1.data(), ctx);
+                }
+            }
+            t = omp_get_wtime() - t;
+            std::cout << "Time in copying/permuting from xyzt to tcxyz (fast) " << t / 10
+                      << " (overhead " << t / 10 / tref << " )" << std::endl;
         }
 
         // Shift tensor 1 on the z-direction and store it on tensor 2
@@ -83,8 +108,8 @@ int main(void) {
         {
             double t = omp_get_wtime();
             for (unsigned int rep = 0; rep < 10; ++rep) {
-                local_contraction<Nd, Nd, 3>("tcxyzs", dim, false, t1.data(), "tCxyzs", dim1, false,
-                                             t2.data(), "tCc", dimc, tc.data(), ctx);
+                local_contraction<Nd, Nd, 3>("tcxyzs", dim1, false, t1.data(), "tCxyzs", dim1,
+                                             false, t2.data(), "tCc", dimc, tc.data(), ctx);
             }
             t = omp_get_wtime() - t;
             std::cout << "Time in contracting " << t / 10 << std::endl;
@@ -107,6 +132,7 @@ int main(void) {
                   << vol1 * 1.0 * sizeof(T) / 1024 / 1024 << " MiB)" << std::endl;
 
         // Copy tensor t0 into tensor 1 (for reference)
+        double tref = 0.0;
         {
             double t = omp_get_wtime();
             for (unsigned int rep = 0; rep < 10; ++rep) {
@@ -116,6 +142,7 @@ int main(void) {
             }
             t = omp_get_wtime() - t;
             std::cout << "Time in dummy copying from xyzts to tcxyzs " << t / 10 << std::endl;
+            tref = t / 10; // time in copying a whole tensor with size dim1
         }
 
 
@@ -131,7 +158,29 @@ int main(void) {
                 }
             }
             t = omp_get_wtime() - t;
-            std::cout << "Time in copying/permuting from xyzts to tcxyzs " << t / 10 << std::endl;
+            std::cout << "Time in copying/permuting from xyzts to tcxyzs " << t / 10
+                      << " (overhead " << t / 10 / tref << " )" << std::endl;
+        }
+
+        // Copy tensor t0 into each of the c components of tensor 1 (fast?)
+        {
+            double t = omp_get_wtime();
+            for (unsigned int rep = 0; rep < 10; ++rep) {
+                for (int c = 0; c < dim[5]; ++c) {
+                    const Coor<Nd - 2> from0 = {0};
+                    const Coor<Nd - 1> from1 = {0, c, 0};
+                    Coor<Nd - 2> dim0a;
+                    std::copy_n(dim0.begin(), Nd - 2, dim0a.begin());
+                    Coor<Nd - 1> dim1a;
+                    std::copy_n(dim1.begin(), Nd - 1, dim1a.begin());
+                    local_copy<Nd - 2, Nd - 1>(
+                        "xyzt", from0, dim0a, dim0a, (const std::array<T, SC> *)t0.data().get(),
+                        ctx, "tcxyz", from1, dim1a, (std::array<T, SC> *)t1.data().get(), ctx);
+                }
+            }
+            t = omp_get_wtime() - t;
+            std::cout << "Time in copying/permuting from xyzt to tcxyz (fast?) " << t / 10
+                      << " (overhead " << t / 10 / tref << " )" << std::endl;
         }
 
         // Shift tensor 1 on the z-direction and store it on tensor 2
@@ -155,7 +204,7 @@ int main(void) {
         {
             double t = omp_get_wtime();
             for (unsigned int rep = 0; rep < 10; ++rep) {
-                local_contraction<Nd, Nd, 3>("tcxyzs", dim, false, t1.data().get(), "tCxyzs", dim1,
+                local_contraction<Nd, Nd, 3>("tcxyzs", dim1, false, t1.data().get(), "tCxyzs", dim1,
                                              false, t2.data().get(), "tCc", dimc, tc.data().get(),
                                              ctx);
             }
